7f33a9e2fa13c019e5ffb60d49a36bdda38eb45a
Alongkon Natphunwat <136414303+nine9031@users.noreply.github.com>
2025-02-17 13:46:54 +0700

    Update Main.ts

--- src/Main.ts
+++ src/Main.ts
@@ -1,20 +1,53 @@
-import { User } from "./User"
-import { Participant } from "./Participant"
-//import { Activity } from "./Activity"
-import { Registration } from "./Registration"
-import { Certificate } from "./Certificate"
-import { Instructor } from "./Instructor"
-console.log("################# User ##################")
-const user1 = new User("Siwakorn", "123", "First", "student", "664259028@webmail.npru.ac.th")
-console.log(user1.toString())
-console.log("#############################################")
-console.log("################# Participant ##################")
-const participant1 = new Participant("Siwakorn", "123", "First", "student", "664259028@webmail.npru.ac.th")
-console.log("#############################################")
-console.log("################# Registration ##################")
-//const registration1 = new Registration("1", participant1, activity1, "student")
-console.log("#############################################")
-console.log("################# User ##################")
-const certificate1 = new Certificate(1, "กิจกรรม", new Instructor(), "Siwakorn", "")
-console.log(user1.toString())
-console.log("#############################################")
+import { User } from "./User";
+import { Participant } from "./Participant";
+import { Registration } from "./Registration";
+import { Instructor } from "./Instructor";
+import { Certificate } from "./Certificate";
+import { Notification } from "./Notification";
+import { Activity } from "./Activity";
+let tae013=new User("Ratchanon","deekub","tae","student","664259013@webmail.npru.ac.th")
+console.log(tae013.toString())
+
+let pa1 = new Participant("Phongsakorn", "112", 'Boom', "student", "664259011@webmail.npru.ac.th")
+
+let pa2 = new Participant("Alongkon", "1234", "Nine", "student", "664259031@webmail.npru.ac.th")
+
+console.log(pa1.toString())
+console.log(pa2.toString())
+// == instructor ==
+const ins = new Instructor('teacher boom', '123', 'Boom', 'teacher', 't@gmail.com')
+
+// === activity ===
+console.log('=== activity ===')
+const ac1 = new Activity(1, 'drawing', 'NongTop', 150, 'วัน 31 ธันวาคม 10:00 - 22:00', 'วัน 28 ถึง 30', 'published', true, true, ins, 'sch')
+
+const ac2 = new Activity(2, 'scubar', 'NongBoom', 3000, 'วัน 31 ธันวาคม 10:00 - 22:00', 'วัน 28 ถึง 30', 'published', true, true, ins, 'sch')
+
+
+console.log('=== activity ===')
+
+console.log('=== registration ===')
+const registrations = []
+
+let reg = new Registration(1, pa1, ac1, 'รออุมัติ')
+registrations.push(reg)
+console.log(reg.toString())
+let reg2 = new Registration(2, pa2, ac2, 'รออนุมัติ')
+registrations.push(reg2)
+console.log(reg2.toString())
+// ac1.approveParticipant(pa1, registrations)
+
+ins.approveParticipant(ac2,pa2, registrations)
+console.log(reg2.toString())
+
+console.log(ac1.generateCertificate(registrations, ins, 'signature', 'drawing'))
+console.log('=== registration ===')
+console.log(ins.issueCertificate(ac2,pa2,registrations,'01'))
+console.log('=== instructor ===')
+// let Api = new Certificate(1,"กิจกรรม",new Instructor(),"ลายเซ็น","")
+
+// console.log(Api.toString())
+
+// let eiei = new Notification(1,"ผมทำได้แล้วครับ เย้", pa1 , "รออนุมัติ")
+
+// console.log(eiei.toString())

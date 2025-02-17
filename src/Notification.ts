c0610988362de91426734e7955e27e6bf37d57d9
Alongkon Natphunwat <136414303+nine9031@users.noreply.github.com>
2025-02-17 13:47:18 +0700

    Update Notification.ts

--- src/Notification.ts
+++ src/Notification.ts
@@ -1,17 +1,19 @@
-import { Participant } from './Participant';
-export class Notification {
-    private notificationId: number
-    private message: string
-    private recipient: Participant
-    private status: string
+import { Participant } from "./Participant"
 
-    constructor(notificationId: number, message: string, recipient: Participant, status: string){
-        this.notificationId = notificationId
-        this.message = message
-        this.recipient = recipient
-        this.status = status
+class Notification {
+    private notification : number
+    private message : string
+    private recipient : Participant
+    private status : string
+
+    constructor(notification:number,message:string,recipient:Participant,status:string){
+        this.notification=notification
+        this.message=message
+        this.recipient=recipient
+        this.status=status
     }
-    public toString(): string{
-        return `Notification [notificationId = ${this.notificationId}, message = ${this.message}, recipient = ${this.recipient}, status = ${this.status}]`
+    public toString():string{
+        return `Notification[notificationId=${this.notification}, message=${this.message}, recipient=${this.recipient}, status=${this.status}]`
     }
 }
+export { Notification}

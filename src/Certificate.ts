dbd3b19e8b3905787b623bec981f315b176f133d
Alongkon Natphunwat <136414303+nine9031@users.noreply.github.com>
2025-02-17 13:45:43 +0700

    Update Certificate.ts

--- src/Certificate.ts
+++ src/Certificate.ts
@@ -1,22 +1,26 @@
-export class Certificate {
-    private certificateId: number
-    private content: string
-    private issuer: Inspecter
-    private signature: File
-    private template: File
+import { Instructor } from "./Instructor"
+class Certificate{
+    private certificateId:number
+    private content:string
+    private issuer:Instructor
+    private signature:string
+    private template:string
+    constructor(certificateId:number,content:string, issuer:Instructor,signature:string,template:string){
+        this.certificateId=certificateId
+        this.content=content
+        this.issuer=issuer
+        this.signature=signature
+        this.template=template
 
-    constructor(certificateId: number, content: string, issuer: Inspecter, signature: File, template: File){ 
-        this.certificateId = certificateId
-        this.content = content
-        this.issuer = issuer
-        this.signature = signature
-        this.template = template
     }
-
-    public genetateCertificate():Certificate {
-        return new Certificate(this.certificateId, this.content, this.issuer, this.signature, this.template)
+    public static generateCertificate(certificateId:number,content:string, issuer:Instructor,signature:string,template:string):Certificate{
+        return new Certificate(certificateId,content,issuer,signature,template)
     }
-    public sendCertificateNotification(): void {
-        
+    public sendCertificateNotification():void{
+        // let pa = new Notification(1,"พร้อมให้ดาวน์โหลดแล้ว",new , 'df')
+    }
+    public toString():string{
+        return `Certificate[certificateId=${this.certificateId}, content=${this. content},issuer=${this.issuer},signature=${this.signature},template=${this.template}]`
     }
 }
+export { Certificate }

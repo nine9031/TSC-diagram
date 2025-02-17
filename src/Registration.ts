1a7bca4a0fb893a5daf1ea069d436b13c6fa8869
Alongkon Natphunwat <136414303+nine9031@users.noreply.github.com>
2025-02-17 13:48:16 +0700

    Update Registration.ts

--- src/Registration.ts
+++ src/Registration.ts
@@ -1,25 +1,39 @@
-import { Activity } from "./Activity"
-import { Participant } from "./Participant"
+import { Activity } from './Activity'
+import { Participant } from './Participant'
 
-export class Registration {
+class Registration {
+
     private registrationId: number
     private participant: Participant
     private activity: Activity
     private status: string
 
-    constructor(registrationId: number, participant: Participant, activity: Activity, status: string) {
+    constructor(registrationId: number, participant: Participant, activity: Activity, status: string){
         this.registrationId = registrationId
         this.participant = participant
         this.activity = activity
         this.status = status
     }
-    public submitRegistration() {
 
+    public getParticipant(): Participant{
+        return this.participant
     }
+
+    public submitRegistration(registrationId: number, participant: Participant, activity: Activity, status: string): void {
+        this.registrationId = registrationId
+        this.participant = participant
+        this.activity = activity
+        this.status = status
+    }
+
     public updateStatus(status: string): void {
         this.status = status
     }
-    public toString() {
-        return `Registration[registrationId = ${this.registrationId}, participant = ${this.participant}, activity = ${this.activity}, status = ${this.status}]`
+
+    public toString():string{
+        return `Registration[registrationId=${this.registrationId}, participant=${this.participant}, activity=${this.activity}, status=${this.status}]`
     }
+
 }
+
+export { Registration }
